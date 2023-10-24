1b65552ea688e3ddffc6dbd30d9b809f752a9657
AndreNussbaumer <57812689+AndreNussbaumer@users.noreply.github.com>
2023-10-24 16:30:10 +0100

    Add files via upload
    responsive design

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
+// Check if user is using mobile phone
+
+let mobile
+
+if(screen.width <= 430){
+  mobile = true
+} else {
+  mobile = false
+}
+
+
 
 window.addEventListener('scroll', function(e) {
 
   let value = window.scrollY
 
-  if (value > 80 ) {
-    document.getElementById("navbar").style.backgroundColor = "black";
-    document.getElementById("navbar").style.height = "100px";
-    document.querySelector(".links").style.top = "20px";
-    document.querySelector(".logo").style.top = "25px";
-    document.querySelector(".social-media").style.top = "35px";
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "initial";
-    document.getElementById("navbar").style.height = "130px";
-    document.querySelector(".links").style.top = "35px";
-    document.querySelector(".logo").style.top = "40px";
-    document.querySelector(".social-media").style.top = "50px";
+  if(!mobile){
+    if (value > 80 ) {
+      document.getElementById("navbar").style.backgroundColor = "black";
+      document.getElementById("navbar").style.height = "100px";
+      document.querySelector(".links").style.top = "20px";
+      document.querySelector(".logo").style.top = "25px";
+      document.querySelector(".social-media").style.top = "35px";
+    } else {
+      document.getElementById("navbar").style.backgroundColor = "initial";
+      document.getElementById("navbar").style.height = "130px";
+      document.querySelector(".links").style.top = "35px";
+      document.querySelector(".logo").style.top = "40px";
+      document.querySelector(".social-media").style.top = "50px";
+    }
   }
 
 })
@@ -22,22 +35,26 @@ window.addEventListener('scroll', function(e) {
 // function has to be separated because the listener is listening to when it's scrolled, but not when page refreshes, so it needs a function that loads the y position when refreshing
 
 function scrollFunction() {
-  
+
   let value = window.scrollY
 
-  if (value > 80 ) {
-    document.getElementById("navbar").style.backgroundColor = "black";
-    document.getElementById("navbar").style.height = "100px";
-    document.querySelector(".links").style.top = "20px";
-    document.querySelector(".logo").style.top = "25px";
-    document.querySelector(".social-media").style.top = "35px";
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "initial";
-    document.getElementById("navbar").style.height = "130px";
-    document.querySelector(".links").style.top = "35px";
-    document.querySelector(".logo").style.top = "40px";
-    document.querySelector(".social-media").style.top = "50px";
+  if(!mobile){
+    if (value > 80 ) {
+      document.getElementById("navbar").style.backgroundColor = "black";
+      document.getElementById("navbar").style.height = "100px";
+      document.querySelector(".links").style.top = "20px";
+      document.querySelector(".logo").style.top = "25px";
+      document.querySelector(".social-media").style.top = "35px";
+    } else {
+      document.getElementById("navbar").style.backgroundColor = "initial";
+      document.getElementById("navbar").style.height = "130px";
+      document.querySelector(".links").style.top = "35px";
+      document.querySelector(".logo").style.top = "40px";
+      document.querySelector(".social-media").style.top = "50px";
+    }
   }
+
+
 }
 
 scrollFunction()
